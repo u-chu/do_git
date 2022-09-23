5c57d3ceb1be0f2884a58dbf9b342df0a353d08a
ghola <ghoul.mask@gmail.com>
2022-09-23 23:03:14 +0500

    2022-09-23

--- do.py
+++ do.py
@@ -13,37 +13,43 @@ from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 # --register-pepper-plugins="HCSFP64.dll"
 
 # url = "https://google.com"
-url='https://ru6.darkorbit.com/'
-# url="https://flashroom.ru/games1821.html"
+# url='https://ru6.darkorbit.com/'
+url="https://flashroom.ru/games1821.html"
+# url='https://tankionline.com/play/'
 
 class bview(QWebEngineView):
- def __init__(self):
+ def __init__(self, ):
   super(bview, self).__init__()
-#   self.b=QWebEngineView()
+  self.p=None
+#         BigpointClient/1.4.6
   self.page().profile().setHttpUserAgent("BigpointClient/1.6.7")
   self.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
-       
+  QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+  QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+  QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+#   self.settings().setAttribute(WebEngineSettings.PluginsEnabled, True)
+      
+
        
 
 class QDO(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(QDO, self).__init__(*args, **kwargs)
-        
-#         BigpointClient/1.4.6
         wdg = QWidget()
-        # wdg.setStyleSheet("border:50ps solid red")
-#         self.setCentralWidget(wdg)
-        lay = QVBoxLayout(wdg)
-#         lay.setStyleSheet("border:10")
-        self.setCentralWidget(wdg)
-        tbb=QTabWidget()
-        tbb.setStyleSheet("border:0px")
 
+        lay = QVBoxLayout(wdg)
+        lay.setSpacing(0)
+        lay.setMargin(0)
+
+        self.setCentralWidget(wdg)
+        self.tbb=QTabWidget()
+
+        self.tbb.setTabsClosable(True)
         self.hscale=QProgressBar(self)
         self.hscale.setMaximum(100)
         self.hscale.setMinimum(0)
         # self.hscale.setValue(55)
-        self.hscale.setFixedHeight(1)
+        self.hscale.setFixedHeight(2)
         self.hscale.setTextVisible(False)
 
         qs=QSettings('do.ini', 'lv')
@@ -52,50 +58,58 @@ class QDO(QMainWindow):
         except:
             pass
         browser = bview()
+
         browser.loadStarted.connect(self.loadStartedHandler)
         browser.loadProgress.connect(self.loadProgressHandler)
         browser.loadFinished.connect(self.loadFinishedHandler)
-        tbb.addTab(browser, "")
+        browser.titleChanged.connect(self.onTitleChanged0)
+        self.tbb.addTab(browser, "")
         wdg.setLayout(lay)
         lay.addWidget(self.hscale)
-        lay.addWidget(tbb)        
+        lay.addWidget(self.tbb)
         self.show()
         browser.load(QUrl(url))
-        
+
+    def onTitleChanged0(self, s):
+      self.tbb.setTabText(0, s)
+  
     def closeEvent(self, event):
         qs=QSettings('do.ini', 'lv')
         qs.setValue("geometry", self.saveGeometry())
         qs.sync()
-        
-#     @QtCore.pyqtSlot()
+
     def loadStartedHandler(self):
         self.hscale.setValue(0)
-#         self.ptToolbar.setVisible(True)
 
-#     @QtCore.pyqtSlot()
     def loadFinishedHandler(self):
         self.hscale.setValue(0)
-#         self.ptToolbar.setVisible(False)
 
-#     @QtCore.pyqtSlot(int)
     def loadProgressHandler(self, prog):
         self.hscale.setValue(prog)
         
-
 if __name__ == '__main__':
-  arg=sys.argv[1:]
-  a1="--register-pepper-plugins=./HCSFP64.dll"
+  arg=sys.argv
+#   a1="--register-pepper-plugins=./HCSFP64.dll"
   a2="--ppapi-flash-path=./HCSFP64.dll"
-#   a3="--register-pepper-plugins=pepflashplayer.dll;application/x-shockwave-flash"
-#   a4="--ppapi-flash-path=./pepflashplayer.dll"
+#   a3='--ppapi-plugin-launcher=./HCSFP64.dll'
+#   a1="--register-pepper-plugins=HCSFP64.dll;application/x-shockwave-flash"  
+#   a1="--register-pepper-plugins=pepflashplayer.dll;application/x-shockwave-flash"
+#   a2="--ppapi-flash-path=pepflashplayer.dll"
   a5="--ppapi-flash-version=26.0.0.137"
 #   arg.append(a1)
   arg.append(a2)
+#   arg.append(a3)
   arg.append(a5)
-  arg.extend(['-platform', 'windows:altgr', 'enable-accelerated-2d-canvas', '--default-background-color=000000ff', '--disable-bundled-ppapi-flash', '--ignore-gpu-blacklist', '--in-process-gpu'])
+  arg.extend(['-platform', 'windows:altgr', 'enable-accelerated-2d-canvas',
+              '--default-background-color=000000ff', '--disable-bundled-ppapi-flash', '--ignore-gpu-blacklist',
+              '--in-process-gpu', '-enable-pepper-testing', '--ppapi', '--ppapi-in-process', '--ppapi-startup-dialog'])
+#               '--remote-debugging-port=9221'])
+
+
 
   print(arg)
   app = QApplication(arg)
+  # app.setStyleSheet("border: 1px solid;")
   ex = QDO()
 
   sys.exit(app.exec_())
